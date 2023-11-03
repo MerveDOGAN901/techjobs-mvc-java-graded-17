5ae4156785810c84cbd529ce38d1ee4076105b93
Merve Dogan <139923400+MerveDOGAN901@users.noreply.github.com>
2023-11-02 19:59:47 -0500

    complete

--- src/test/java/org/launchcode/techjobsmvc/TestTaskFour.java
+++ src/test/java/org/launchcode/techjobsmvc/TestTaskFour.java
@@ -97,7 +97,7 @@ public class TestTaskFour {
                 .andExpect(xpath("//table[contains(@class, 'job-listing')][1]").exists())
                 .andExpect(xpath("//table[contains(@class, 'job-listing')][2]").exists())
                 .andExpect(xpath("//table[contains(@class, 'job-listing')][3]").exists())
-                .andExpect(xpath("//table[contains(@class, 'job-listing')][1]").doesNotExist());
+                .andExpect(xpath("//table[contains(@class, 'job-listing')][4]").doesNotExist());
     }
 
 }
